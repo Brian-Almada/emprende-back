1dbeccd3a787fee8a7830ef28fd9ade7e02838e3
Brian Almada <brianalexisalmada84@gmail.com>
2024-04-23 20:24:43 -0300

    Completar la funcion requestController con sus condicionales y variables

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ require('dotenv').config()
 
 const http = require('http')
 
-function requestController() {
-    console.log('request recibida')
-}
+function requestController(req, res) {
+    const url = req.url
+    const method = req.method
+    console.log({ url, method })
 
+    if (method === 'GET' && url === "/") {
+        res.setHeader("content-type", "text/html; charset=utf-8")
+        res.write("<h1>¡Hola mundo!</h1>")
+        res.end()
+        return
+    }
+
+    if (method === 'GET' && url === "/about") {
+        res.setHeader("content-type", "text/html; charset=utf-8")
+        res.write("<h1>Sobre mi</h1>")
+        res.end()
+        return
+    }
+
+    res.setHeader("content-type", "text/html; charset=utf-8")
+    res.write("<h1>404</h1>")
+    res.end()
+}
 server = http.createServer(requestController)
 
 const PORT = process.env.PORT
