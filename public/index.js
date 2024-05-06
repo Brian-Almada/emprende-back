7234101724fec6f4e280842a4e0b7b70f595d04c
Brian Almada <brianalexisalmada84@gmail.com>
2024-05-06 15:10:42 -0300

    Crear botones de obtencion y creacion de tareas

--- public/index.js
+++ public/index.js
@@ -1,14 +1,12 @@
-const agregar_texto = document.querySelector('.agregar-texto');
-const contenedor_texto = document.querySelector('.contenedor-texto');
+const getBtn = document.querySelector(".get-tasks");
+const createBtn = document.querySelector(".create-tasks");
+const input = document.querySelector(".task-name");
 
-agregar_texto.addEventListener("click", crearTexto)
+getBtn.addEventListener("click", function () {
+    console.log("GET TAREAS");
+    fetch("http://localhost:4000/api/tasks")
+});
 
-function crearTexto() {
-    const textoHTML = `<p>Esta semana dos hermanos fueron atacados fuertemente por el dengue. La pasaron mal por una semana y volvieron el mismo día a sus respectivos trabajos.</p>
-    <p>Lo gracioso? Ninguno de los dos hermanos sabía que estaban viviendo exactamente lo mismo como si de una telenovela escrita por alguien se tratara.</p>
-    <p> TREMENDO este Dengue!!!</p>
-    `;
-
-    contenedor_texto.innerHTML = textoHTML;
-}
-
+createBtn.addEventListener("click", function () {
+    console.log("CREAR TAREAS");
+})
