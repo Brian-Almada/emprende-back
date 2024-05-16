f928d37c7092e8cee45ee23e5d859c50813739c5
Brian Almada <brianalexisalmada84@gmail.com>
2024-05-15 21:09:11 -0300

    Agregar el evento submit del form

--- public/login.js
+++ public/login.js
@@ -6,6 +6,17 @@ const codeBtn = document.querySelector('.codeBtn')
 const baseBackendUrl = `${window.origin}/api`
 
 codeBtn.addEventListener('click', async function (e) {
+    console.log("Pidiendo código")
+
+    const res = await fetch(
+        `${baseBackendUrl}/auth/login/${inputEmail.value}/code`,
+        {
+            method: 'POST',
+        }
+    )
+    const resJSON = await res.json()
+})
+form.addEventListener('submit', async function (e) {
     e.preventDefault()
     console.log("Pidiendo código")
 
